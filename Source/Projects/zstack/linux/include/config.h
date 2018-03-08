0c370dff81a2da9012694382f120edaec4403eee
VectorShen <sqj@anchorelec.com>
2018-03-08 16:45:20 +0800

    增加了一个include路径和config.h文件，用来全局配置，定义全局管道路径

---
+++ Source/Projects/zstack/linux/include/config.h
@@ -0,0 +1,46 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define NPI_IPC_LISTEN_PIPE_SERVER2CLIENT   "/home/centos/fifos/npi_ipc_server2client"
+#define NPI_IPC_LISTEN_PIPE_CLIENT2SERVER   "/home/centos/fifos/npi_ipc_client2server"
+
+#define NPI_IPC_LISTEN_PIPE_CHECK_STRING    "connect_to_npi_ipc"
+
+#define ZLSZNP_LISTEN_PIPE_SERVER2CLIENT    "/home/centos/fifos/zlsznp_server2client"
+#define ZLSZNP_LISTEN_PIPE_CLIENT2SERVER    "/home/centos/fifos/zlsznp_client2server"
+
+#define ZLSZNP_LISTEN_PIPE_CHECK_STRING     "connect_to_zlsznp"
+
+#define NWKMGR_LISTEN_PIPE_SERVER2CLIENT    "/home/centos/fifos/nwkmgr_server2client"
+#define NWKMGR_LISTEN_PIPE_CLIENT2SERVER    "/home/centos/fifos/nwkmgr_client2server"
+
+#define NWKMGR_LISTEN_PIPE_CHECK_STRING     "connect_to_nwkmgr"
+
+#define OTASERVER_LISTEN_PIPE_SERVER2CLIENT "/home/centos/fifos/otaserver_server2client"
+#define OTASERVER_LISTEN_PIPE_CLIENT2SERVER "/home/centos/fifos/otaserver_client2server"
+
+#define OTASERVER_LISTEN_PIPE_CHECK_STRING  "connect_to_otaserver"
+
+#define GATEWAY_LISTEN_PIPE_SERVER2CLIENT   "/home/centos/fifos/gateway_server2client"
+#define GATEWAY_LISTEN_PIPE_CLIENT2SERVER   "/home/centos/fifos/gateway_client2server"
+
+#define GATEWAY_LISTEN_PIPE_CHECK_STRING    "connect_to_gateway"
+
+#define SERVER_NPI_IPC                      "srv_npi_ipc"
+#define SERVER_ZLSZNP                       "srv_zlsznp"
+#define SERVER_NWKMGR                       "srv_nwkmgr"
+#define SERVER_OTASERVER                    "srv_otaserver"
+#define SERVER_GATEWAY                      "srv_gateway"
+
+#define SERVER_LISTEN_BUF_SIZE			    20
+
+typedef enum
+{
+    SERVER_NPI_IPC_ID,
+    SERVER_ZLSZNP_ID,
+    SERVER_NWKMGR_ID,
+    SERVER_OTASERVER_ID,
+    SERVER_GATEWAY_ID
+}emServerId;
+
+#endif /* _CONFIG_H_ */
